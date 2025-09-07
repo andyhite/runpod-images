0fac5fcf48aa366dbf625a3f08377ca8efcc7cff
Andy Hite <andrew.hite@circuit.ai>
2025-09-07 15:51:01 -0500

    fix: comfyui image startup issues

--- services/comfyui/start.sh
+++ services/comfyui/start.sh
@@ -1,64 +1,60 @@
 #!/bin/bash
 set -e
 
-export INVOKEAI_ROOT=/invokeai
-export HF_HOME=${HF_HOME:-$INVOKEAI_ROOT/.cache/huggingface}
-export MPLCONFIGDIR=${MPLCONFIGDIR:-$INVOKEAI_ROOT/.matplotlib}
-
 # ---------------------------------------------------------------------------- #
 #                          Function Definitions                                #
 # ---------------------------------------------------------------------------- #
 
 # Setup ssh
 setup_ssh() {
-  if [[ $PUBLIC_KEY ]]; then
-    echo "Setting up SSH..."
-    apt-get update
-    apt-get install -y openssh-server
+    if [[ $PUBLIC_KEY ]]; then
+        echo "Setting up SSH..."
+        apt-get update
+        apt-get install -y openssh-server
 
-    mkdir -p ~/.ssh
-    echo "$PUBLIC_KEY" >> ~/.ssh/authorized_keys
-    chmod 700 -R ~/.ssh
+        mkdir -p ~/.ssh
+        echo "$PUBLIC_KEY" >>~/.ssh/authorized_keys
+        chmod 700 -R ~/.ssh
 
-    if [ ! -f /etc/ssh/ssh_host_rsa_key ]; then
-      ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -q -N ''
-      echo "RSA key fingerprint:"
-      ssh-keygen -lf /etc/ssh/ssh_host_rsa_key.pub
+        if [ ! -f /etc/ssh/ssh_host_rsa_key ]; then
+            ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -q -N ''
+            echo "RSA key fingerprint:"
+            ssh-keygen -lf /etc/ssh/ssh_host_rsa_key.pub
+        fi
+
+        if [ ! -f /etc/ssh/ssh_host_dsa_key ]; then
+            ssh-keygen -t dsa -f /etc/ssh/ssh_host_dsa_key -q -N ''
+            echo "DSA key fingerprint:"
+            ssh-keygen -lf /etc/ssh/ssh_host_dsa_key.pub
+        fi
+
+        if [ ! -f /etc/ssh/ssh_host_ecdsa_key ]; then
+            ssh-keygen -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key -q -N ''
+            echo "ECDSA key fingerprint:"
+            ssh-keygen -lf /etc/ssh/ssh_host_ecdsa_key.pub
+        fi
+
+        if [ ! -f /etc/ssh/ssh_host_ed25519_key ]; then
+            ssh-keygen -t ed25519 -f /etc/ssh/ssh_host_ed25519_key -q -N ''
+            echo "ED25519 key fingerprint:"
+            ssh-keygen -lf /etc/ssh/ssh_host_ed25519_key.pub
+        fi
+
+        service ssh start
+
+        echo "SSH host keys:"
+        for key in /etc/ssh/*.pub; do
+            echo "Key: $key"
+            ssh-keygen -lf "$key"
+        done
     fi
-
-    if [ ! -f /etc/ssh/ssh_host_dsa_key ]; then
-      ssh-keygen -t dsa -f /etc/ssh/ssh_host_dsa_key -q -N ''
-      echo "DSA key fingerprint:"
-      ssh-keygen -lf /etc/ssh/ssh_host_dsa_key.pub
-    fi
-
-    if [ ! -f /etc/ssh/ssh_host_ecdsa_key ]; then
-      ssh-keygen -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key -q -N ''
-      echo "ECDSA key fingerprint:"
-      ssh-keygen -lf /etc/ssh/ssh_host_ecdsa_key.pub
-    fi
-
-    if [ ! -f /etc/ssh/ssh_host_ed25519_key ]; then
-      ssh-keygen -t ed25519 -f /etc/ssh/ssh_host_ed25519_key -q -N ''
-      echo "ED25519 key fingerprint:"
-      ssh-keygen -lf /etc/ssh/ssh_host_ed25519_key.pub
-    fi
-
-    service ssh start
-
-    echo "SSH host keys:"
-    for key in /etc/ssh/*.pub; do
-      echo "Key: $key"
-      ssh-keygen -lf $key
-    done
-  fi
 }
 
 # Export env vars
 export_env_vars() {
-  echo "Exporting environment variables..."
-  printenv | grep -E '^RUNPOD_|^PATH=|^_=' | awk -F = '{ print "export " $1 "=\"" $2 "\"" }' >> /etc/rp_environment
-  echo 'source /etc/rp_environment' >> ~/.bashrc
+    echo "Exporting environment variables..."
+    printenv | grep -E '^RUNPOD_|^PATH=|^_=' | awk -F = '{ print "export " $1 "=\"" $2 "\"" }' >>/etc/rp_environment
+    echo 'source /etc/rp_environment' >>~/.bashrc
 }
 
 # ---------------------------------------------------------------------------- #
